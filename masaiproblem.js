5a7076946f06a8eb36f39ce7bf00c1cdeba3aac3
Devendra singh <devendrasingh20025@gmail.com>
2025-01-25 23:03:14 +0530

    this is the week 2

---
+++ masaiproblem.js
@@ -0,0 +1,32 @@
+// q=11
+// let age=50;
+
+// let bag=(0>=age)?"Invalid Age":(age>=60)?"Eligible for Senior Discount":"Not Eligible for Senior Discount"
+// console.log(bag)
+
+
+//q12
+
+// function larNum(num1,num2,num3){
+
+// let bag=(num1>num2)?(num1>num3)?num1:num3:(num2>num3)?num2:num3
+// return bag
+// }
+
+// console.log(larNum(200,50,400))
+
+
+//q13
+
+// function priceTag(price){
+
+// let bag=(price>=1000)?"Expensive":(price>=500 && price<=999)?"Moderate":(price<500 && price>0)?"Affordable":(price==0)?"Free":"Invalid price"
+
+// return bag
+// }
+
+// console.log(priceTag(-1))
+
+
+
+
---
+++ masaiproblem1.js
@@ -0,0 +1,43 @@
+//q11
+
+// let book={
+
+//     title:"The Hoit",
+//     author:"J.R.R Tok",
+//     year:1957
+// }
+// let bag=""
+// for(let key in book){
+
+//     bag+=`${key}:${book[key]}`+" "
+
+// }
+// console.log(bag)
+
+//q12
+
+// let str="aabacdeg";
+
+// let obj={}
+
+// for(let i=0;i<=str.length-1;i++){
+
+//     if(obj[str[i]]){
+//          obj[str[i]]++
+//     }else{
+//         obj[str[i]]=1
+//     }
+// }
+
+
+
+// console.log(obj)
+
+function findMatching(n,m,str,pat){
+    // write code here
+    
+}
+
+findMatching(5 ,3,
+    "acbab",
+    "bab")
